81fc2ddbf3aeada98e059ea1499bf0451ded3c3b
Wzyan <3101885298@qq.com>
2022-08-18 14:50:00 +0800

    kkb-down-core | 更改课程级别的去缓存策略 目前改为直接读取 output 中的目录，从而做到只要【上传完就删，一定不会重复读取】

--- src/getFilesArr_1.js
+++ src/getFilesArr_1.js
@@ -1,46 +1,29 @@
 import nodeApi from '../utils/node-api.js';
 import path from 'path';
 import fs from 'fs';
-import { exec, execSync } from 'child_process'
-import * as configs from '../config/index.js';
-import {
-    filterName,
-    eachObj
-} from '../utils/index.js';
-import { checkPath } from '../utils/node-api.js';
 let allArrObj = {
 
 }
 let courseDir = 'output';
-let outRootDir = ''
-eachObj(configs.default, async (key, configInfo) => {
-    const {
-        basePath,
-        courseName,
-        accessToken,
-        Authorization,
-        cookie,
-        course_id,
-    } = configInfo
-    debugger
-    let rootDir = __dirname.replace('/dist', '')
-    debugger
-    outRootDir = rootDir
-    let coursePath = path.resolve(rootDir, basePath);
-    let isExit = await nodeApi.fileIsExist(coursePath)
-    // 如果目录存在才继续生成
-    if (isExit) {
+let rootDir = __dirname.replace('/dist', '')
+let outDir = path.resolve(rootDir, courseDir)
+const files = fs.readdirSync(outDir)
+for (let i = 0; i < files.length; i++) {
+    const courseName = files[i]
+    const stat = fs.lstatSync(dirPath + '/' + courseName)
+    if (stat.isDirectory() === true) {
+        let coursePath = path.resolve(outDir, courseName);
         let arr = nodeApi.loadFileNameByPath4Ext(coursePath, ['txt'], (item) => {
             item[1].unshift(courseDir)
             return item
         }, [])
         allArrObj[courseName] = arr
     }
-})
+}
 // 清空下载内容
 // deleteFileInDir('/Users/wzyan/Documents/selfspace/ffmpeg-download/Web前端面试涨薪名企培养计划', ['mp4'])
 // 生成的数据结构存储的文件地址
-let commandArrTxtPath = `${outRootDir}/result.js`
+let commandArrTxtPath = `${rootDir}/result.js`
 
 let content = `
         export default ${JSON.stringify(allArrObj)}
