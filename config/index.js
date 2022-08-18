deff98753d92ccc993176779cbd26da9afd65191
Wzyan <3101885298@qq.com>
2022-08-18 14:58:15 +0800

    kkb-down-core | 包名处理

--- config/index.js
+++ config/index.js
@@ -11,6 +11,6 @@ for (const key of ctx.keys()) {
     modules[name] = module.default;
 }
 // 配置 2
-export const bdypDir = 'tangdy' // 在百度云盘上对应的文件夹名称
+export const bdypDir = 'baozihi426' // 在百度云盘上对应的文件夹名称
 export default modules
 
