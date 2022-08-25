aca9d1ba1dcbeb007c65375b99eed13dfb930e6f
Wzyan <3101885298@qq.com>
2022-08-25 22:06:30 +0800

    feat: 脚本控制

--- retry.sh
+++ retry.sh
@@ -1,12 +1,4 @@
+npm run formatConfig
 # 1. 进行webpack编译
 npm run build
 npm run build1
-npm run build
-# 2. 根据当前output内剩余课程的txt
-npm run build2
-npm run build
-# 3. 再次生成数据结构并再次生成download.sh
-npm run build3
-npm run build
-# 4. 执行download.sh进行重传
-npm run build4-linux
