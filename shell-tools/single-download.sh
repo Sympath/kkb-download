e89bb599d4e1befe64c58545957750615dc5d6b1
Wzyan <3101885298@qq.com>
2022-08-29 16:20:02 +0800

    feat: shellcourseName

--- shell-tools/single-download.sh
+++ shell-tools/single-download.sh
@@ -6,11 +6,11 @@ while read line; do
 done <./config/single-download-config
 
 echo "获取到的地址为：${m3u8Url}"
-ffmpeg -i $m3u8Url -c copy -bsf:a aac_adtstoasc ./${filename}.mp4
+ffmpeg -i $m3u8Url -c copy -bsf:a aac_adtstoasc ./${courseName}.mp4
 echo "下载完成"
-bypy upload ./${filename}.mp4 ${bypyDir}/${bypyFullDir}
+bypy upload ./${courseName}.mp4 ${bypyDir}/${bypyFullDir}
 echo "上传云盘完成，邮件通知下"
-rm -rf ./${filename}.mp4
+rm -rf ./${courseName}.mp4
 echo "删除资源完成"
-node src/mail.js --name=${bdypDir} --courseName=${filename}
+node src/mail.js --name=${bdypDir} --courseName=${courseName}
 echo "邮件通知成功，可访问地址确定：https://pan.baidu.com/disk/main?from=homeFlow#/index?category=all&path=%2Fapps%2Fbypy%2F${bypyDir}/${bypyFullDir}✅"
